b6c709e3322c0d3501359ac1ee00b878ef83b9c6
Jeff <118331364+Jerbert16@users.noreply.github.com>
2023-02-07 21:28:52 -0700

    connected APIs, now retrieves/displays weather info

--- assets/script.js
+++ assets/script.js
@@ -26,38 +26,74 @@
 // Save former inputs as buttons > append > commit to local storage > make cickable w/event listener & event target
 const userInputEl = document.querySelector("#userInput");
 const searchBtnEl = document.querySelector("#searchBtn");
+const currentWeatherEl = document.querySelector("#currentWthr");
 let btnEls = document.querySelector("#formEl");
 
 const apiKey = "e741dcb38a3d668e1bd5bc73c1c15c13";
 const gpsAPI =
   "http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}";
 
-
-
 function getGpsCoord(event) {
   let userInput = userInputEl.value;
   event.preventDefault();
 
- let cityBtn = document.createElement("button");
- cityBtn.innerText = userInput;
- let addCtyBtn = btnEls.append(cityBtn);
-// probably need to add eventtarget...
+  let cityBtn = document.createElement("button");
+  cityBtn.innerText = userInput;
+  let addCtyBtn = btnEls.append(cityBtn);
 
-  console.log(userInput);
+
 
   let gpsQueryString =
     "http://api.openweathermap.org/geo/1.0/direct?q=" +
     userInput +
     "&limit=1&appid=" +
     apiKey;
+
   fetch(gpsQueryString)
     .then(function (response) {
       return response.json();
     })
     .then(function (data) {
       for (let index = 0; index < data.length; index++) {
-        console.log(data[index].lat);
-        console.log(data[index].lon);
+        let currentWeatherUrl =
+          "https://api.openweathermap.org/data/2.5/weather?lat=" +
+          data[index].lat +
+          "&lon=" +
+          data[index].lon +
+          "&appid=" +
+          apiKey +
+          "&units=imperial";
+
+        fetch(currentWeatherUrl)
+          .then(function (response) {
+            return response.json();
+          })
+          .then(function (weatherData) {
+            // let dateEl = document.createElement("h3");
+            // dateEl.setAttribute("id", "date");
+            // let theDate = document.querySelector("#date");
+
+            let tempEl = document.createElement("h4");
+            tempEl.setAttribute("id", "temp");
+            currentWeatherEl.appendChild(tempEl);
+            let currentTemp = document.querySelector("#temp");
+
+            currentTemp.textContent = "Temp: " + weatherData.main.temp + "°F";
+
+            let windEl = document.createElement("h4");
+            windEl.setAttribute("id", "wind");
+            currentWeatherEl.appendChild(windEl);
+            let wind = document.querySelector("#wind");
+
+            wind.textContent = "Wind: " + weatherData.wind.speed + "MPH";
+
+            let humidityEl = document.createElement("h4");
+            humidityEl.setAttribute("id", "humidity");
+            currentWeatherEl.appendChild(humidityEl);
+            let humidity = document.querySelector("#humidity");
+
+            humidity.textContent = "Humidity: " + weatherData.main.humidity + "%";
+          });
       }
     });
 }
